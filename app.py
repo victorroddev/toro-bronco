a0ff2c05d1c12f691b92d9bac0538a5214c55412
victorroddev <javier.rod.dev@gmail.com>
2025-07-30 14:40:27 -0600

    update

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+#Supported Languages
+SUPPORTED_LANGUAGES = ['en', 'es']
+DEFAULT_LANGUAGE = 'es'
+
+def get_user_language():
+    accept_languages = request.accept_languages.best_match(SUPPORTED_LANGUAGES)
+    if accept_languages:
+        return accept_languages
+    return DEFAULT_LANGUAGE
+
 @app.route("/")
 def home():
-    return render_template('home.html')
-
+    lang = get_user_language()
+    return render_template(f'{lang}/home.html')
 
 @app.route("/menu")
 def menu():
     return render_template('menu.html')
 
+@app.route("/ubicacion")
+def ubicacion():
+    return render_template('ubicacion.html')
+
+@app.route('/presite')
+def presite():
+    return render_template('presite.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5500)
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,29 +1,55 @@
 addEventListener('DOMContentLoaded',  function(){
     const menuOpen = document.getElementById('menu-open');
     const menuClose = document.getElementById('menu-close');
-    const menu = document.getElementById('menu');
+    const menu = document.getElementById('menu-modal');
+    const menuIcon = this.document.getElementById('menu');
+    const firstLine = document.getElementById('first-line');
+    const secondLine = document.getElementById('second-line');
+    const thirdLine = document.getElementById('third-line')
 
-    menuOpen.addEventListener('click', function(){
-        menu.classList.remove('hidden');
-        menuOpen.classList.add('hidden');
-        menuClose.classList.remove('hidden')
+
+    menuIcon.addEventListener('click', function(){
+        menu.classList.toggle('hidden');
+        firstLine.classList.toggle('absolute');
+        firstLine.classList.toggle('rotate-45');
+        firstLine.classList.toggle('top-1-2');
+        firstLine.classList.toggle('rotate-left-0');
+        firstLine.classList.toggle('-translate-y-1/2');
+        thirdLine.classList.toggle('opacity-0');
+        secondLine.classList.toggle('absolute');
+        secondLine.classList.toggle('-rotate-45');
+        secondLine.classList.toggle('top-1-2');
+        secondLine.classList.toggle('rotate-left-0');
+        secondLine.classList.toggle('-translate-y-1/2');
+
+
+        setTimeout(() => {
+            menu.classList.toggle('opacity-0');
+            menu.classList.toggle('opacity-100')
+        }, 200);
+    })
+
+    // menuOpen.addEventListener('click', function(){
+    //     menu.classList.remove('hidden');
+    //     menuOpen.classList.add('hidden');
+    //     menuClose.classList.remove('hidden')
         
-        setTimeout(() => {
-            menu.classList.remove('opacity-0');
-            menu.classList.add('opacity-100')
-        }, 100);
+    //     setTimeout(() => {
+    //         menu.classList.remove('opacity-0');
+    //         menu.classList.add('opacity-100')
+    //     }, 100);
     
-    })
+    // })
 
-    menuClose.addEventListener('click', function() {
+    // menuClose.addEventListener('click', function() {
 
-        menu.classList.remove('max-h-[900px]', 'opacity-100');
-        menu.classList.add('opacity-0');  
+    //     menu.classList.remove('max-h-[900px]', 'opacity-100');
+    //     menu.classList.add('opacity-0');  
 
-        setTimeout(() => {
-            menu.classList.remove('hidden');
-            menuClose.classList.add('hidden');
-            menuOpen.classList.remove('hidden');
-        }, 100);
-    })
+    //     setTimeout(() => {
+    //         menu.classList.remove('hidden');
+    //         menuClose.classList.add('hidden');
+    //         menuOpen.classList.remove('hidden');
+    //     }, 100);
+    // })
 })
