6df2e2da92cd08d40fded759832f1c2671359a3f
victorroddev <javier.rod.dev@gmail.com>
2025-09-29 18:57:41 -0600

    update changes

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart 
+import base64
+import os
 
 app = Flask(__name__)
+
+TURNSTILE_SECRET_KEY = os.environ.get('TURNSTILE_SECRET_KEY')
+TEST_KEY = ""
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
 
 #Supported Languages
 SUPPORTED_LANGUAGES = ['en', 'es']
@@ -82,6 +92,52 @@ def tests():
     lang = get_user_language()
     return render_template(f'{lang}/tests.html')
 
+
+@app.route('/contacto', methods=['GET','POST'])
+def email():
+    """Function that that manages the contact form
+    """
+    if request.method == 'POST':
+        token = request.form.get('cf-turnstile-response')
+        if not token:
+            return "error, Turnstile, token not found", 400
+        
+        payload = {
+            'secret': TEST_KEY,
+            'response': token,
+            'remoteip': request.remote_addr
+        }
+
+        response = request.post('', data=payload)
+        result = response.json
+
+        if result.get('sucess'):
+            encoded_password = os.environ.get('MAIL_PASSWORD')
+            password = base64.b64code(encoded_password).decode()
+
+            subject = "Mensaje en Toro bronco Real" 
+            name = request.form["name"]
+            email = request.form["email"]
+            phone = request.form["phone"]
+            msg = request.form["msg"]
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("growy.noti@gmail.com", password )
+
+            message  = MIMEText(f"subject: {subject}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {msg}")
+
+            message["from"] = "growy.noti@gmail.com"
+            message["to"] = "bernalrefugio24@gmail.com"
+            message["subject"] = subject
+
+            server.sendmail("growy.noti@gmail.com", "bernalrefugio24@gmail.com", message.as_string())
+
+            server.quit()
+
+        return render_template('home.html')
+    else:
+        return "Error, turnstile failed"
 
 
 if __name__ == '__main__':
